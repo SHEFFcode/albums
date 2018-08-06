c25fb4886121a9a5ce7f968e9c0ef99bc04b827a
SHEFFmachine <sheff@sheffmachine.com>
2018-08-05 22:06:16 -0700

    Add album artwork and make everything scrollable

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -8,26 +8,49 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  headerTextStyle: {
+    fontSize: 18,
+  },
   thumbnailStyle: {
     height: 50,
     width: 50,
   },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
 }
 
 export default ({ album }) => {
-  const { thumbnail_image, title, artist } = album
-  const { headerContentStyle, thumbnailStyle } = styles
+  const { thumbnail_image, title, artist, image } = album
+  const {
+    headerContentStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle,
+  } = styles
 
   return (
     <Card>
       <CardSection>
-        <View style={headerContentStyle}>
+        <View style={thumbnailContainerStyle}>
           <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
-        <View>
-          <Text> {title} </Text>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}> {title} </Text>
           <Text> {artist} </Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   )
